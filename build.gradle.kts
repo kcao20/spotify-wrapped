a935a3f32882aca952c24ee6810c64f29d521e3c
kcao20 <kcao2022@gmail.com>
2024-03-07 18:07:37 -0500

    Add code formatter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,29 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidApplication) apply false
+    id("com.diffplug.spotless") version "6.25.0"
+}
+
+spotless {
+    ratchetFrom("origin/main")
+
+    format("misc") {
+        target("*.gradle", ".gitattributes", ".gitignore")
+
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
+
+    java {
+        target("app/src/*/java/**/*.java")
+
+        importOrder()
+        removeUnusedImports()
+        cleanthat()
+
+        palantirJavaFormat("2.40.0").style("AOSP").formatJavadoc(true)
+
+        formatAnnotations()
+    }
 }
