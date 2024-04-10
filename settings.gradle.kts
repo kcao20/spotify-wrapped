73fef74456c3494355f50c1eb6c42ec3f25cde64
sthompson304 <sthompson304@gatech.edu>
2024-04-10 13:19:34 -0400

    Modified gradle config settings for my build config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,11 @@
 pluginManagement {
+    resolutionStrategy {//for sarah's build
+        eachPlugin {
+            if (requested.id.id == "com.android.application") {
+                useVersion("8.2.1")
+            }
+        }
+    }
     repositories {
         google {
             content {
@@ -18,6 +25,9 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-
+plugins {
+    id("com.android.application") version "8.2.1" apply false
+    // or for library projects: id("com.android.library") version "8.3.1" apply false
+}
 rootProject.name = "Spotify Wrapped"
 include(":app")
