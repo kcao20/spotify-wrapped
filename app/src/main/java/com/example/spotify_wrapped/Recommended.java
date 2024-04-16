a0b8541da1ce7d665487e01a0a7de1186771ebf6
Ltillery03 <113254724+Ltillery03@users.noreply.github.com>
2024-04-16 01:04:23 -0400

    filled in methods to get songs
    Methods to get songs and to store them in objects are complete along with getting the recommendations themselves(I'm pretty sure). all in one file tho.

--- app/src/main/java/com/example/spotify_wrapped/Recommended.java
+++ app/src/main/java/com/example/spotify_wrapped/Recommended.java
@@ -1,9 +1,20 @@
 package com.example.spotify_wrapped;
+import android.view.View;
 import android.widget.ImageView;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+
 public class Recommended {
-private class Song {
+public class Song {//nested class containing parts of a song
 private String name;
 private String artist;
 private String imgURL;
@@ -15,7 +26,7 @@ public Song(String name, String artist, String imgURL) {
     this.imgURL = imgURL;
 }
 
-public String getSong() {
+public String getName() {
     //returns song name
     return name;
 }
@@ -28,11 +39,79 @@ public String getImgURL() {
 }
 public void loadIMG(ImageView imageView) {
     Picasso.get()
-            .load(imageURL)
+            .load(imgURL)
             .placeholder(R.drawable.ic_launcher_background)
             .error(R.drawable.ic_notifications_black_24dp)
             .into(imageView);
 }
 
 }
+
+//create instance to access API
+    private Retrofit retrofit = new Retrofit.Builder()
+        .baseUrl("http://api.spotify.com")
+    //.baseURL("http://api.spotify.com")
+    .addConverterFactory(GsonConverterFactory.create())
+        .build();
+
+public void getRecommendations(final RecListener recListener) {
+    //create instance to access API interface
+    SpotifyService spotifyService = retrofit.create(SpotifyService.class);
+    //call API to get recommendations
+    Call<RecommendationResponse> call = spotifyService.getRecommendations();
+    call.enqueue(new retrofit2.Callback<RecommendationResponse>() {
+        @Override
+        public void onResponse(Call<RecommendationResponse> call, Response<RecommendationResponse> response) {
+            if (response.isSuccessful()) {
+                RecommendationResponse recommendationResponse = response.body();
+                List<Song> songs = processRecommendations(recommendationResponse);
+                //success, go to listener
+                recListener.onRecFetched(songs);
+
+            } else {
+                //need to display error here
+                recListener.onRecFail();
+
+            }
+        }
+
+        @Override
+        public void onFailure(Call<RecommendationResponse> call, Throwable t) {
+            recListener.onRecFail();//notify failure
+        }
+    });
+
+}
+public class RecommendationResponse{ //song used to classify response as an object
+    private List<Song> songs;
+    public List<Song> getSongs() {
+        return songs;
+    }
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
+}
+public interface SpotifyService{ //interface containing methods to get recommendations
+    @GET("recommendations")
+    Call<RecommendationResponse> getRecommendations();
+}
+public interface RecListener{
+    view onRecFetched(List<Song> recommendations);
+    view onRecFail();
+}
+public List<Song> processRecommendations(RecommendationResponse r) { //handles recommended songs
+    List<Song> sonRecs = new ArrayList<>(); //arraylist to store them in
+    if (r != null && r.getSongs() != null) { //makes sure it's not null
+        for (Song rec : r.getSongs()) {
+            String name = rec.getName();
+            String artist = rec.getArtist();
+            String imgURL = rec.getImgURL();
+            Song track = new Song(name, artist, imgURL); //creates a new song object with each song's characteristics
+
+            sonRecs.add(track); //adds it to the list of recommendations
+        }
+    }
+    return sonRecs; //returns list of recommendations
+}
+
 }
