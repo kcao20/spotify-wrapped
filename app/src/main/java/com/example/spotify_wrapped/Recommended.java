c58b3ba6ea36cb90cce2d0c5632090e393d9c734
Ltillery03 <113254724+Ltillery03@users.noreply.github.com>
2024-04-15 21:30:11 -0400

    consolidated song class
    also added method to load image

--- app/src/main/java/com/example/spotify_wrapped/Recommended.java
+++ app/src/main/java/com/example/spotify_wrapped/Recommended.java
@@ -1,14 +1,18 @@
 package com.example.spotify_wrapped;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
 
 public class Recommended {
 private class Song {
 private String name;
 private String artist;
+private String imgURL;
 
-public Song(String name, String artist) {
+public Song(String name, String artist, String imgURL) {
     //creates new instance of song class
     this.name = name;
     this.artist = artist;
+    this.imgURL = imgURL;
 }
 
 public String getSong() {
@@ -19,7 +23,16 @@ public String getArtist() {
     //returns artist name
     return artist;
 }
+public String getImgURL() {
+    return imgURL;
 }
-private class TrackImage {
+public void loadIMG(ImageView imageView) {
+    Picasso.get()
+            .load(imageURL)
+            .placeholder(R.drawable.ic_launcher_background)
+            .error(R.drawable.ic_notifications_black_24dp)
+            .into(imageView);
+}
+
 }
 }
